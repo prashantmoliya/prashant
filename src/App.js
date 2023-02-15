f06ffbb2c70b30bd1f5902a07ba63d2f16a3dccc
prashantmoliya <prashantmoliya@gmail.com>
2023-02-16 02:15:53 +0530

    array prectice

--- src/App.js
+++ src/App.js
@@ -3,20 +3,60 @@ import './App.css';
 
 function App() {
 
-    const array= [10,15,20,25,30,35,40,45,50];
+    // const arr= [10,15,20,25,30,35,40,45,50];
 
-    // console.log("array++",array);
-    // array.push(55);  //Last Add
-    // array.pop();  //Last Remove
-    // array.shift();  //First Remove
-    // array.unshift(5);  //First Add
-    array.indexOf();
+    // const arr= [35,45,40,25,20,15,10,30,50];  // (2). No. Line ma set
+
+    // const arr1= ["React js is a "];  // (3). Join Two Array
+    // const arr2= ["javascript library."];  
+    // const ans= arr1.concat(arr2);  
+
+    // const arr= [10,15,20,25,30,35,40,45,50];  // (4). Reverse (Reverse / Join Mix)
+    // const r= arr.reverse(); 
+    // const j= arr.join("-"); 
+
+    // const arr = ["Php", "Node", "Javascript","Reactjs","Flutter"];  // (5). Convert Array to String
+    // const A= arr.toString();   
+    
+    // const arr = ["Php", "Node", "Javascript","Reactjs","Flutter"];  // (6). Length
+    // const L= arr.length;
+    
+    // const arr = ["Php", "Node", "Javascript","Reactjs","Flutter"];  // (7). Index of
+    // const i= arr.indexOf("Reactjs");
+
+
+
+    console.log("array++",arr); 
+
+
+    // arr.push(55);  //Last Add
+    // arr.pop();  //Last Remove
+    // arr.shift();  //First Remove
+    // arr.unshift(5);  //First Add
+
+    // arr.sort();  // (2). No. Line ma set
+
+    // console.log(ans);  // (3). Join Two Array
+    // console.log(arr1.concat(arr2));  
 
     
 
   return (
     <div className="App">
+        {/* <div> <b>Step-1 :-</b>  {ans}</div> */}
+        {/* <div> <b>Step-2 :-</b>  {arr1.concat(arr2)}</div> */}
 
+        {/* <div> <b>Reverse :-</b> {r}</div> */}
+        {/* <div> <b>Reverse / Join :-</b> {r,j}</div> */}
+
+        {/* <div> <b>Array to String Convert :-</b> {A}</div> */}
+
+        {/* <div> <b>Length :-</b> {L}</div> */}
+
+        {/* <div> <b>Index of :-</b> {i}</div> */}
+
+
+    
 
     </div>
   );
