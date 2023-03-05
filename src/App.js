770fda57a1e8303a993cf401418709954ffa1f1c
prashantmoliya <prashantmoliya@gmail.com>
2023-03-05 17:02:37 +0530

    17-02. -Merge/ -Spread/ -destructure

--- src/App.js
+++ src/App.js
@@ -1,132 +1,151 @@
 import logo from './logo.svg';
 import './App.css';
 
- // 16-02-23/ Thursday
-    // -Map and Key
-    // -Filter and reduce
-
-    // Using this day(-filter / -includes / -reduce)
+  // 17-02-23/ Friday
+    // -Merging
+    // -Spreading
+    // -destructuring
 
 function App() {
 
- // 1- Filter 
+ // 1- Merging (total 1-step)
 
-    // const list= [
-    //     {
-    //         id : "2303",
-    //         name : "Mouse",
-    //         description : "Mouse is computer fast work.",
-    //         quantity : 2,
-    //         price : 600,
-    //     },
-    //     { id : "2153", name : "Vefar", description : "Poteto Vefar balaji fever.", quantity : 5, price : 20, },
-    //     { id : "5623", name : "volyboll", description : "volyboll is smart ball.", quantity : 10, price : 700, },
-    //     { id : "7856", name : "Keyboard", description : "Key-board is tying process.", quantity : 10, price : 750, },
-    //     { id : "9564", name : "Vadapav", description : "Vadapav enjoy watching time movie.", quantity : 25, price : 100, },
-    //     { id : "0125", name : "Protocol", description : "Key-board protocol blue color.", quantity : 16, price : 200, },
-    //     { id : "9658", name : "Pendrive", description : "32GB Pendrive 1peice.", quantity : 8, price : 530, },
+  // method-1 (two Array object join)
+
+    // const cart= [
+    //   { id : "1234", name : "mehul", cart : "pen", qty : 2, price : 10, },
+    //   { id : "2356", name : "raj", cart : "pencil", qty : 4, price : 5, },
+    //   { id : "2356", name : "nikil", cart : "rabar", qty : 1, price : 5, },
     // ];
 
-    // console.log("List++", list);
-
-
-
-
- // 2- includes
-
-    // const list= [
-    //     {
-    //         id : "2303",
-    //         name : "Mouse",
-    //         description : "Mouse is computer fast work.",
-    //         quantity : 2,
-    //         price : 600,
-    //     },
-    //     { id : "2153", name : "Vefar", description : "Poteto Vefar balaji fever.", quantity : 5, price : 20, },
-    //     { id : "5623", name : "volyboll", description : "volyboll is smart ball.", quantity : 10, price : 700, },
-    //     { id : "7856", name : "Keyboard", description : "Key-board is tying process.", quantity : 10, price : 750, },
-    //     { id : "9564", name : "Vadapav", description : "Vadapav enjoy watching time movie.", quantity : 25, price : 100, },
-    //     { id : "0125", name : "Protocol", description : "Key-board protocol blue color.", quantity : 16, price : 200, },
-    //     { id : "9658", name : "Pendrive", description : "32GB Pendrive 1peice.", quantity : 8, price : 530, },
+    // const cart2= [
+    //   { id : "1234", name : "om", cart : "stel", qty : 3, price : 10, },
+    //   { id : "2356", name : "naman", cart : "pencil", qty : 4, price : 5, },
+    //   { id : "2356", name : "jay", cart : "sancho", qty : 2, price : 5, },
     // ];
 
-    // console.log("List++", list);
+    // const MergeArray= cart.concat(cart2);    // step-3 (variable thi merge)
+
+    // console.log("Cart++", cart,cart2);    // step-1 (simple) 
+    // console.log("Cart++", cart.concat(cart2));    // step-2 (merge)
+    // console.log("Cart++", MergeArray);    // step-3 (variable thi merge)
+
+
+  // method-2 (two Array element join)
+  
+    // const fruits= ["f1","f2","f3"];
+    // const fruits2= ["f4","f5","f6"];
+
+    // const MergeArray= fruits.concat(fruits2);    // step-3 (variable thi merge)
+    
+    // console.log("Fruits++", fruits,fruits2);    // step-1 (simple)
+    // console.log("Fruits++", fruits.concat(fruits2));    // step-2 (merge)
+    // console.log("Fruits++", MergeArray);    // step-3 (variable thi merge)
+
+
+  // method-3 (three Array element join)
+
+    // const fruits= ["f1","f2","f3"];
+    // const fruits2= ["f4","f5","f6"];
+    // const fruits3= ["f7","f8","f9"];
+
+    // const MergeArray= fruits.concat(fruits2).concat(fruits3);    // step-3 (variable thi merge)
+
+    // console.log("Fruits++", fruits,fruits2,fruits3);    // step-1 (simple)
+    // console.log("Fruits++", fruits.concat(fruits2).concat(fruits3));    // step-2 (merge)
+    // console.log("Fruits++", MergeArray);    // step-3 (variable thi merge)
 
 
 
 
- // 3- reduce
+ // 2- Spreading (total 2-step)
 
-  // method-1 (simple reduce)
+  // method-1 (1- Array chuto kre, 2- Array object join)
 
-    // const list= [
-    //     {
-    //         id : "2303",
-    //         name : "Mouse",
-    //         description : "Mouse is computer fast work.",
-    //         quantity : 2,
-    //         price : 600,
-    //     },
-    //     { id : "2153", name : "Vefar", description : "Poteto Vefar balaji fever.", quantity : 5, price : 20, },
-    //     { id : "5623", name : "volyboll", description : "volyboll is smart ball.", quantity : 10, price : 700, },
-    //     { id : "7856", name : "Keyboard", description : "Key-board is tying process.", quantity : 10, price : 750, },
-    //     { id : "9564", name : "Vadapav", description : "Vadapav enjoy watching time movie.", quantity : 25, price : 100, },
-    //     { id : "0125", name : "Protocol", description : "Key-board protocol blue color.", quantity : 16, price : 200, },
-    //     { id : "9658", name : "Pendrive", description : "32GB Pendrive 1peice.", quantity : 8, price : 530, },
+    // const cart= [
+    //   { id : "1234", name : "mehul", cart : "pen", qty : 2, price : 10, },
+    //   { id : "2356", name : "raj", cart : "pencil", qty : 4, price : 5, },
+    //   { id : "2356", name : "nikil", cart : "rabar", qty : 1, price : 5, },
     // ];
 
-    // console.log("List++", list);
-
-    // const totalquantity= list.reduce((acc,curr)=>{
-    //     return acc + curr.quantity
-    // },0)
-
-    // const totalprice= list.reduce((acc,curr)=>{
-    //     return acc + curr.price
-    // },0)
-
-    // const total= list.reduce((acc,curr)=>{
-    //     return acc + curr.quantity * curr.price
-    // },0)
-
-
-
-  // method-2 (filter & reduce mix)
-
-    // const list= [
-    //     {
-    //         id : "2303",
-    //         name : "Mouse",
-    //         description : "Mouse is computer fast work.",
-    //         quantity : 2,
-    //         price : 600,
-    //     },
-    //     { id : "2153", name : "Vefar", description : "Poteto Vefar balaji fever.", quantity : 5, price : 20, },
-    //     { id : "5623", name : "volyboll", description : "volyboll is smart ball.", quantity : 10, price : 700, },
-    //     { id : "7856", name : "Keyboard", description : "Key-board is tying process.", quantity : 10, price : 750, },
-    //     { id : "9564", name : "Vadapav", description : "Vadapav enjoy watching time movie.", quantity : 25, price : 100, },
-    //     { id : "0125", name : "Protocol", description : "Key-board protocol blue color.", quantity : 16, price : 200, },
-    //     { id : "9658", name : "Pendrive", description : "32GB Pendrive 1peice.", quantity : 8, price : 530, },
+    // const cart2= [
+    //   { id : "1234", name : "om", cart : "stel", qty : 3, price : 10, },
+    //   { id : "2356", name : "naman", cart : "pencil", qty : 4, price : 5, },
+    //   { id : "2356", name : "jay", cart : "sancho", qty : 2, price : 5, },
     // ];
 
-    // console.log("List++", list);
+    // const SpreadArray= [...cart,...cart2];    // step-4 (variable thi spread Array ma / sem-merge)
 
-    // const a= list.filter((i)=> i.quantity > 3)    //(method 1 - qty)
-    // // const a= list.filter((i)=> i.quantity > 3 && i.quantity < 20)    //(method 2 - qty)
-    // // const a= list.filter((i)=> i.price > 500)    //(method 3 - price)
-    // // const a= list.filter((i)=> i.price >= 50 && i.price <= 600)    //(method 4 - price)
+    // console.log("Cart++ 1", cart,cart2);    // step-1 (simple)
+    // console.log("Cart++ 2", cart.concat(cart2));    // step-2 (merge)
+    // console.log("Cart++ 3", ...cart,...cart2);    // step-3 (spread)
+    // console.log("Cart++ 4", SpreadArray);    // step-4 (variable thi spread Array ma / sem-merge)
 
-    // const totalquantity= a.reduce((acc,curr)=>{
-    //     return acc + curr.quantity
-    // },0)
 
-    // const totalprice= a.reduce((acc,curr)=>{
-    //     return acc + curr.price
-    // },0)
+  // method-2 (1- Array chuto kre, 2- Array element join)
 
-    // const total= a.reduce((acc,curr)=>{
-    //     return acc + curr.quantity * curr.price
-    // },0)
+    // const fruits= ["f1","f2","f3"];
+    // const fruits2= ["f4","f5","f6"];
+
+    // const SpreadArray= [...fruits,...fruits2];    // step-4 (variable thi spread Array ma / sem-merge)
+
+    // console.log("Fruits++ 1", fruits,fruits2);    // step-1 (simple)
+    // console.log("Fruits++ 2", fruits.concat(fruits2));    // step-2 (merge)
+    // console.log("Fruits++ 3", ...fruits,...fruits2);    // step-3 (spread)
+    // console.log("Fruits++ 4", SpreadArray);    // step-4 (variable thi spread Array ma / sem-merge)
+
+
+  // method-3 (1- Array chuto kre, 2- Array element join)
+
+    // const fruits= ["f1","f2","f3"];
+    // const fruits2= ["f4","f5","f6"];
+    // const fruits3= ["f7","f8","f9"];
+
+    // const SpreadArray= [...fruits,...fruits2,...fruits3];    // step-4 (variable thi spread Array ma / sem-merge)
+
+    // console.log("Fruits++ 1", fruits,fruits2,fruits3);    // step-1 (simple)
+    // console.log("Fruits++ 2", fruits.concat(fruits2).concat(fruits3));    // step-2 (merge)
+    // console.log("Fruits++ 3", ...fruits,...fruits2,...fruits3);    // step-3 (spread)
+    // console.log("Fruits++ 4", SpreadArray);    // step-4 (variable thi spread Array ma / sem-merge)
+
+
+
+
+ // 3- destructuring (total 2-step)
+
+  // method-1 (Array chuto kre)
+
+    // const fruits= ['f1','f2','f3'];    // 1 
+    // const [fruits1,fruits2]= ['f1','f2','f3'];    // 2
+    //     // const [test1,test2]= ['f1','f2','f3'];    // 2
+    // const [fruits1,fruits2,fruits3]= ['f1','f2','f3'];    // 3 
+    // const [fruits1,fruits2,fruits3,fruits4]= ['f1','f2','f3'];    // 4  
+
+
+    // console.log("Fruits++ 1", fruits);    // step-1 (simple)
+    // console.log("Fruits++ 2", fruits1, fruits2);    // step-2 (two variable - length 3)
+    //     // console.log("Fruits++ 2", test1, test2);    // step-2 (variable name koi pan chale)
+    // console.log("Fruits++ 3", fruits1, fruits2,fruits3);    // step-3 (three variable - length 3)
+    // console.log("Fruits++ 4", fruits1, fruits2, fruits3, fruits4);    // step-4 (four variable - length 3 (last variable ni value undefine--tema 4-length nthi aetle undefine))
+
+
+  // method-2 (Object chuto kre)
+
+    // const person= {    // step-1 (simple)
+    //     name : "naman",
+    //     age : 23,
+    // }
+
+    // const {name,age}= {    // step-2 (destructure / Object chuto kre)
+    //   name : "raj",
+    //   age : 20,
+    // } 
+
+    // console.log("Person++ 1", person);    // step-1 (simple)
+    // console.log("Person++ 2", name, age);    // step-2 (destructure / Object chuto kre)
+    // console.log("Person++ 3 ", {name, age});    // step-3 (destructure / Object ma (Key & value sathe)) 
+    // console.log("Person++ 4", [name, age]);    // step-4 (destructure / Array ma (element string ma))
+    // console.log("Person++ 5", [{name, age}]);    // step-5 (destructure / Array and Object ma)
 
 
 
@@ -134,175 +153,39 @@ function App() {
   return (
     <div className="App">
 
-    {/* 1. Filter */}
+    {/* 1. Merging */}
+         
+         {/* Syntax :- */}
+                      {/* a.concat(b) */}
 
-         {/* <h2>Product List</h2>
- 
-         <table>
-             <thead>
-                 <tr>
-                     <td>Sr no.</td>
-                     <td>Id</td>
-                     <td>Product Name</td>
-                     <td>Description</td>
-                     <td>Quantity</td>
-                     <td>Price</td>
-                 </tr>
-             </thead>
-             <tbody>
-                 {
-                     list.filter((i)=> i.quantity > 3).map((i,index)=>{    //(method 1 - qty)
-                     // list.filter((i)=> i.quantity > 3 && i.quantity < 20).map((i,index)=>{    //(method 2 - qty)
-                     // list.filter((i)=> i.price > 500).map((i,index)=>{    //(method 3 - price)
-                     // list.filter((i)=> i.price >= 50 && i.price <= 600).map((i,index)=>{    //(method 4 - price)
-                         return(
-                             <tr>
-                                 <td> <b>{index + 1+"."}</b> </td>
-                                 <td> {i.id} </td>
-                                 <td> {i.name} </td>
-                                 <td> {i.description} </td>
-                                 <td> {i.quantity} </td>
-                                 <td> {i.price} </td>
-                             </tr>
-                         )
-                     })
-                 }
-             </tbody>
-         </table> */}
+         {/* -total 1-step */}
+                      {/* a.concat(b) (Array object/element join) */}
+                      
+
+
+    {/* 2. Spreading */}
+
+         {/* Syntax :- */}
+                      {/* ...a,...b  */}
+
+         {/* -total 2-step */}
+                      {/* ...a,...b (Array chuto kre) */}
+                      {/* [...a,...b] (Array object/element join) */}
 
 
 
+    {/* 3. destructuring */}
 
-    {/* 2. includes */}
+         {/* Syntax :- */}
+                      {/* concat [x,y]= ["admin1","admin2"] */}
+                      {/* concat {id,no}= { id : "om", no : 02} */}
 
-         {/* <h2>Product List</h2>
- 
-         <table>
-             <thead>
-                 <tr>
-                     <td>Sr no.</td>
-                     <td>Id</td>
-                     <td>Product Name</td>
-                     <td>Description</td>
-                     <td>Quantity</td>
-                     <td>Price</td>
-                 </tr>
-             </thead>
-             <tbody>
-                 {
-                     list.filter((i)=> i.id.includes('9')).map((i,index)=>{    //(method 1 - id)
-                     // list.filter((i)=> i.name.includes('P')).map((i,index)=>{    //(method 2 - name)
-                     // list.filter((i)=> i.description.includes('32')).map((i,index)=>{    //(method 3 - description)
-                         return(
-                             <tr>
-                                 <td> <b>{index + 1+"."}</b> </td>
-                                 <td> {i.id} </td>
-                                 <td> {i.name} </td>
-                                 <td> {i.description} </td>
-                                 <td> {i.quantity} </td>
-                                 <td> {i.price} </td>
-                             </tr>
-                         )
-                     })
-                 }
-             </tbody>
-         </table> */}
+         {/* -total 2-step */}
+                      {/* concat [a1,a2]= ["admin1","admin2"] (Array chuto kre) */}
+                      {/* concat {name,age}= { name : "om", age : 12} (Object chuto kre) */}
 
 
 
-
-    {/* 3. reduce */}
-
-       {/* method-1 (simple reduce) */}
-
-         {/* <h2>Product List</h2>
- 
-         <table>
-             <thead>
-                 <tr>
-                     <td>Sr no.</td>
-                     <td>Id</td>
-                     <td>Product Name</td>
-                     <td>Description</td>
-                     <td>Quantity</td>
-                     <td>Price</td>
-                     <td>Total</td>
-                 </tr>
-             </thead>
-             <tbody>
-                 {
-                     list.map((i,index)=>{    
-                         return(
-                             <tr>
-                                 <td> <b>{index + 1+"."}</b> </td>
-                                 <td> {i.id} </td>
-                                 <td> {i.name} </td>
-                                 <td> {i.description} </td>
-                                 <td> {i.quantity} </td>
-                                 <td> {i.price} </td>
-                                 <td> <b>{i.quantity * i.price}</b> </td>
-                             </tr>
-                         )
-                     })
-                 }
-                 <tr>
-                    <td colSpan={2}> <b>Total</b> </td>
-                    <td> <b>-</b> </td>
-                    <td> <b>-</b> </td>
-                    <td> <b>{totalquantity}</b> </td>
-                    <td> <b>{totalprice}</b> </td>
-                    <td> <b>{total}</b> </td>
-                 </tr>
-             </tbody>
-         </table> */}
-
-
-
-       {/* method-2 (filter & reduce mix) */}
-
-         {/* <h2>Product List</h2>
- 
-         <table>
-             <thead>
-                 <tr>
-                     <td>Sr no.</td>
-                     <td>Id</td>
-                     <td>Product Name</td>
-                     <td>Description</td>
-                     <td>Quantity</td>
-                     <td>Price</td>
-                     <td>Total</td>
-                 </tr>
-             </thead>
-             <tbody>
-                 {
-                     list.filter((i)=> i.quantity > 3).map((i,index)=>{    //(method 1 - qty)   
-                     // list.filter((i)=> i.quantity > 3 && i.quantity < 20).map((i,index)=>{    //(method 2 - qty)  
-                     // list.filter((i)=> i.price > 500).map((i,index)=>{    //(method 3 - price) 
-                     // list.filter((i)=> i.price >= 50 && i.price <= 600).map((i,index)=>{    //(method 4 - price) 
-                         return(
-                             <tr>
-                                 <td> <b>{index + 1+"."}</b> </td>
-                                 <td> {i.id} </td>
-                                 <td> {i.name} </td>
-                                 <td> {i.description} </td>
-                                 <td> {i.quantity} </td>
-                                 <td> {i.price} </td>
-                                 <td> <b>{i.quantity * i.price}</b> </td>
-                             </tr>
-                         )
-                     })
-                 }
-                 <tr>
-                    <td colSpan={2}> <b>Total</b> </td>
-                    <td> <b>-</b> </td>
-                    <td> <b>-</b> </td>
-                    <td> <b>{totalquantity}</b> </td>
-                    <td> <b>{totalprice}</b> </td>
-                    <td> <b>{total}</b> </td>
-                 </tr>
-             </tbody>
-         </table> */}
 
 
          
